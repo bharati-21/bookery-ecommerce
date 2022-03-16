d12d1ffa1cd8265f67e4b1274e5b33df790ae64c
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-16 17:02:42 +0530

    feat: Navbar, Landing Page and Footer components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import "./styles/styles.css"
+import "./styles/styles.css";
+import { Navbar } from './components/Navbar';
+
+import { Footer } from './components/Footer';
+import { LandingPage } from "./pages/LandingPage";
 
 function App() {
     return (
         <div className="App">
-        
+            <Navbar  />
+            <LandingPage  />
+            <Footer  />
         </div>
     );
 }
