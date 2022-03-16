5620e2e268fb8295b5171f67196dba65499a87ec
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-16 19:31:49 +0530

    feat: starter components for all basic routes

---
+++ src/pages/Logout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logout = () => {
+  return (
+    <div className="page-wrapper flex-col flex-align-center flex-justify-center">This is Logout Page</div>
+  )
+}
+
+export { Logout };
---
+++ src/pages/Products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div className="page-wrapper flex-col flex-align-center flex-justify-center">This is Products Page</div>
+  )
+}
+
+export { Products }
