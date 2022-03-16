f31f2c8b8deb803d54dff113547a7afbcc513022
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-16 18:34:56 +0530

    fix: change class to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,58 +2,58 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer class="footer home-footer" id="footer">
-            <div class="primary-footer grid grid-5 py-2-5 px-5">
-                <span class="icon">
-                    <i class="fab fa-cc-visa"></i>
+        <footer className="footer home-footer" id="footer">
+            <div className="primary-footer grid grid-5 py-2-5 px-5">
+                <span className="icon">
+                    <i className="fab fa-cc-visa"></i>
                 </span>
-                <span class="icon">
-                    <i class="fab fa-cc-mastercard"></i>
+                <span className="icon">
+                    <i className="fab fa-cc-mastercard"></i>
                 </span>
-                <span class="icon">
-                    <i class="fab fa-paypal"></i>
+                <span className="icon">
+                    <i className="fab fa-paypal"></i>
                 </span>
-                <span class="icon">
-                    <i class="fab fa-amazon-pay"></i>
+                <span className="icon">
+                    <i className="fab fa-amazon-pay"></i>
                 </span>
-                <span class="icon">
-                    <i class="fab fa-google-pay"></i>
+                <span className="icon">
+                    <i className="fab fa-google-pay"></i>
                 </span>  
             </div>
-            <div class="secondary-footer px-5 py-2 grid grid-4">
-                <ul class="footer-item list list-stacked list-style-none grid-item">
-                    <p class="list-head text-sm p-0-5 mb-1">
+            <div className="secondary-footer px-5 py-2 grid grid-4">
+                <ul className="footer-item list list-stacked list-style-none grid-item">
+                    <p className="list-head text-sm p-0-5 mb-1">
                         Customer Center
                     </p>
-                    <li class="list-item flex-row px-0-5">
-                        <span class="icon">
-                            <i class="fas fa-phone-alt mr-0-5"></i>
+                    <li className="list-item flex-row px-0-5">
+                        <span className="icon">
+                            <i className="fas fa-phone-alt mr-0-5"></i>
                         </span> 
                         Support Helpline
                     </li>
-                    <li class="list-item text-sm px-0-5">
-                        <a href="mailto:bookery@elixir.com" class="btn btn-link btn-light mr-0-5">
-                            <span class="icon">
-                                <i class="fas fa-envelope"></i>
+                    <li className="list-item text-sm px-0-5">
+                        <a href="mailto:bookery@elixir.com" className="btn btn-link btn-light mr-0-5">
+                            <span className="icon">
+                                <i className="fas fa-envelope"></i>
                             </span>
                             Email Us
                         </a>
                     </li>
-                    <li class="list-item text-sm px-0-5">
+                    <li className="list-item text-sm px-0-5">
                         Weekdays 09:30 - 18:00
                     </li>
-                    <li class="list-item text-sm px-0-5">
+                    <li className="list-item text-sm px-0-5">
                         Weekends 11:00 - 16:00
                     </li>
-                    <li class="list-item text-sm px-0-5">
+                    <li className="list-item text-sm px-0-5">
                         Holdays: Sat, Sun, National Holidays
                     </li>  
                 </ul>
-                <ul class="footer-item list list-stacked list-style-none grid-item">
-                    <p class="list-head text-sm p-0-5 mb-1">
+                <ul className="footer-item list list-stacked list-style-none grid-item">
+                    <p className="list-head text-sm p-0-5 mb-1">
                         Service Hub and Store
                     </p>
-                    <li class="list-item flex-row px-0-5">
+                    <li className="list-item flex-row px-0-5">
                         <address>
                             Royal Industrial Estate, <br />
                             Opposite Robin & Co., <br />
@@ -62,60 +62,60 @@ const Footer = () => {
                         </address>
                     </li>
                 </ul>
-                <ul class="footer-item list list-stacked list-style-none grid-item">
-                    <p class="list-head text-sm p-0-5 mb-1">
+                <ul className="footer-item list list-stacked list-style-none grid-item">
+                    <p className="list-head text-sm p-0-5 mb-1">
                         Quick Links
                     </p>
-                    <li class="list-item px-0-5">
-                        <a href="/" class="btn btn-link btn-light text-sm pb-0-25 brand logo">bookery</a>
+                    <li className="list-item px-0-5">
+                        <a href="/" className="btn btn-link btn-light text-sm pb-0-25 brand logo">bookery</a>
                     </li>
-                    <li class="list-item px-0-5 text-xs">
-                        <ul class="list list-inline list-style-none">
-                            <li class="list-item pr-0-25">
-                                <a href="#" class="btn-link">About Us</a> 
+                    <li className="list-item px-0-5 text-xs">
+                        <ul className="list list-inline list-style-none">
+                            <li className="list-item pr-0-25">
+                                <a href="#" className="btn-link">About Us</a> 
                             </li>
-                            <li class="list-item px-0-25">
-                                <a href="#" class="btn-link">Products</a>
+                            <li className="list-item px-0-25">
+                                <a href="#" className="btn-link">Products</a>
                             </li>
-                            <li class="list-item px-0-25">
-                                <a href="#" class="btn-link">My orders</a>
+                            <li className="list-item px-0-25">
+                                <a href="#" className="btn-link">My orders</a>
                             </li>
-                            <li class="list-item pl-0-25">
-                                <a href="#" class="btn-link">Cart</a>
+                            <li className="list-item pl-0-25">
+                                <a href="#" className="btn-link">Cart</a>
                             </li>
                         </ul>
                     </li>
                 </ul>
-                <ul class="footer-item list list-stacked list-style-none grid-item">
-                    <p class="list-head text-sm p-0-5 mb-1">
+                <ul className="footer-item list list-stacked list-style-none grid-item">
+                    <p className="list-head text-sm p-0-5 mb-1">
                         Social Media
                     </p>
-                    <li class="list-item">
-                        <div class="social-links flex-row">
-                            <a href="#" class="btn btn-link btn-light">
-                                <span class="icon">
-                                    <i class="fab fa-twitter"></i>
+                    <li className="list-item">
+                        <div className="social-links flex-row">
+                            <a href="#" className="btn btn-link btn-light">
+                                <span className="icon">
+                                    <i className="fab fa-twitter"></i>
                                 </span>
                             </a>
-                            <a href="#" class="btn btn-link btn-light">
-                                <span class="icon">
-                                    <i class="fab fa-instagram"></i>
+                            <a href="#" className="btn btn-link btn-light">
+                                <span className="icon">
+                                    <i className="fab fa-instagram"></i>
                                 </span>
                             </a>
-                            <a href="#" class="btn btn-link btn-light">
-                                <span class="icon">
-                                    <i class="fab fa-facebook"></i>
+                            <a href="#" className="btn btn-link btn-light">
+                                <span className="icon">
+                                    <i className="fab fa-facebook"></i>
                                 </span>
                             </a>
-                            <a href="#" class="btn btn-link btn-light">
-                                <span class="icon">
-                                    <i class="fab fa-youtube"></i>
+                            <a href="#" className="btn btn-link btn-light">
+                                <span className="icon">
+                                    <i className="fab fa-youtube"></i>
                                 </span>
                             </a>
                         </div>
                     </li>
                 </ul>
-                <div class="mt-1 pt-2 gray-color copyright-section span-cols text-sm text-center">
+                <div className="mt-1 pt-2 gray-color copyright-section span-cols text-sm text-center">
                     &copy; elixir designs 2022
                 </div>
             </div>                       
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -11,111 +11,111 @@ const LandingPage = () => {
     return (
         <>
             <Header  />
-            <main class="main-home my-2 mx-auto py-2 px-5">
-                <h3 class="hero-sub-head text-center mt-1 mb-2">
+            <main className="main-home my-2 mx-auto py-2 px-5">
+                <h3 className="hero-sub-head text-center mt-1 mb-2">
                     Now, get your books delivered within 24 hours to your doorstep!
                 </h3>
-                <section class="section-cateogies my-3 pt-2 pb-3 text-center" id="categories">
-                    <h2 class="section__head">
+                <section className="section-cateogies my-3 pt-2 pb-3 text-center" id="categories">
+                    <h2 className="section__head">
                         Popular Genres
                     </h2>
-                    <p class="section__lead my-2">
+                    <p className="section__lead my-2">
                         Check out the popular genres we have to offer!
                     </p>
-                    <article class="category-container">
-                        <div class="category card card-wt-badge">
-                            <img src={nonFiction} alt="Non-Fiction Book Stacked" class="category-img img-responsive" />
-                            <span class="badge badge-primary py-0-5 my-0-75 mx-0-25 px-0-75">
+                    <article className="category-container">
+                        <div className="category card card-wt-badge">
+                            <img src={nonFiction} alt="Non-Fiction Book Stacked" className="category-img img-responsive" />
+                            <span className="badge badge-primary py-0-5 my-0-75 mx-0-25 px-0-75">
                                 Hot Sale
                             </span>
-                            <div class="card-content">
-                                <h3 class="card-heading mb-1">Non Fiction</h3>
+                            <div className="card-content">
+                                <h3 className="card-heading mb-1">Non Fiction</h3>
                             </div>
                         </div>
-                        <div class="category card">
-                            <img src={fiction} alt="Fiction Book" class="category-img img-responsive" />
-                            <div class="card-content">
-                                <h3 class="card-heading mb-1">Fiction</h3>
+                        <div className="category card">
+                            <img src={fiction} alt="Fiction Book" className="category-img img-responsive" />
+                            <div className="card-content">
+                                <h3 className="card-heading mb-1">Fiction</h3>
                             </div>
                         </div>
-                        <div class="category card">
-                            <img src={classics} alt="Classic - Pride and Prejudice" class="category-img img-responsive" />
-                            <div class="card-content">
-                                <h3 class="card-heading mb-1">Classics</h3>
+                        <div className="category card">
+                            <img src={classics} alt="Classic - Pride and Prejudice" className="category-img img-responsive" />
+                            <div className="card-content">
+                                <h3 className="card-heading mb-1">Classics</h3>
                             </div>
                         </div>
-                        <div class="category card card-wt-badge">
-                            <img src={mystery} alt="Thriller Books" class="category-img img-responsive" />
-                            <div class="card-content">
-                                <h3 class="card-heading mb-1">Mystery</h3>
+                        <div className="category card card-wt-badge">
+                            <img src={mystery} alt="Thriller Books" className="category-img img-responsive" />
+                            <div className="card-content">
+                                <h3 className="card-heading mb-1">Mystery</h3>
                             </div>
                         </div>
-                        <div class="category card card-wt-badge">
-                            <img src={fantasy} alt="Fantasy - A court of mist and fury" class="category-img img-responsive" />
-                            <div class="card-content">
-                                <h3 class="card-heading mb-1">Fantasy</h3>
+                        <div className="category card card-wt-badge">
+                            <img src={fantasy} alt="Fantasy - A court of mist and fury" className="category-img img-responsive" />
+                            <div className="card-content">
+                                <h3 className="card-heading mb-1">Fantasy</h3>
                             </div>
                         </div>
-                        <div class="category card card-wt-badge">
-                            <img src={romance} alt="Romance Books" class="category-img img-responsive" />
-                            <div class="card-content">
-                                <h3 class="card-heading mb-1">Romance</h3>
+                        <div className="category card card-wt-badge">
+                            <img src={romance} alt="Romance Books" className="category-img img-responsive" />
+                            <div className="card-content">
+                                <h3 className="card-heading mb-1">Romance</h3>
                             </div>
                         </div>
                         
                     </article>
-                    <a href="/pages/products/" class="btn text-center mx-auto mt-3 py-0-5 px-0-75">
+                    <a href="/pages/products/" className="btn text-center mx-auto mt-3 py-0-5 px-0-75">
                         Discover more genres! 
-                        <span class="icon">
-                            <i class="fas fa-chevron-right"></i>
+                        <span className="icon">
+                            <i className="fas fa-chevron-right"></i>
                         </span>
                     </a>
                 </section>
-                <section class="section-top-picks my-3 pt-2 pb-3 text-center" id="top-picks">
-                    <h2 class="section__head">
+                <section className="section-top-picks my-3 pt-2 pb-3 text-center" id="top-picks">
+                    <h2 className="section__head">
                         Top Picks
                     </h2>
-                    <p class="section__lead my-2">
+                    <p className="section__lead my-2">
                         Find out what books are loved by all!
                     </p>
-                    <article class="top-picks-container">
-                        <section class="product-catalog">
-                            <article class="products-container grid grid-autofit">
-                                <div class="product-card card card-vertical">
+                    <article className="top-picks-container">
+                        <section className="product-catalog">
+                            <article className="products-container grid grid-autofit">
+                                <div className="product-card card card-vertical">
                                     <a href="/pages/products/">
-                                        <div class="card-header p-1">
+                                        <div className="card-header p-1">
                                             <img src={atomicHabits} alt="Atomic habits by James Clear" />
                                         </div>
-                                        <div class="card-body px-0-75 pb-0-75">
-                                            <div class="card-description my-0-25">
-                                                <p class="card-title text-reg">Atomic Habits</p>
-                                                <p class="text-xs card-subtitle">James Clear</p>
+                                        <div className="card-body px-0-75 pb-0-75">
+                                            <div className="card-description my-0-25">
+                                                <p className="card-title text-reg">Atomic Habits</p>
+                                                <p className="text-xs card-subtitle">James Clear</p>
                                             </div>            
                                         </div>
                                     </a>
                                 </div>
-                                <div class="product-card card card-vertical">
+                                <div className="product-card card card-vertical">
                                     <a href="/pages/products/">
-                                        <div class="card-header p-1">
+                                        <div className="card-header p-1">
                                             <img src={aManCalledOve} alt="Man Called Ove by Fredrick Backman" />
                                         </div>
-                                        <div class="card-body px-0-75">
-                                            <div class="card-description my-0-25">
-                                                <p class="card-title text-reg">A Man Called Ove</p>
-                                                <p class="text-xs card-subtitle">Fredrick Backman</p>
+                                        <div className="card-body px-0-75">
+                                            <div className="card-description my-0-25">
+                                                <p className="card-title text-reg">A Man Called Ove</p>
+                                                <p className="text-xs card-subtitle">Fredrick Backman</p>
                                             </div>            
                                         </div>
                                     </a>        
                                 </div>
                             
-                                <div class="product-card card card-vertical">
-                                    <div class="card-header p-1">
+                                <div className="product-card card card-vertical">
+                                    <div className="card-header p-1">
                                         <img src={shadowAndBone} alt="Shadow and Bone by Leigh Bardugo" />
                                     </div>
-                                    <div class="card-body px-0-75">
-                                        <div class="card-description my-0-25">
-                                            <h6 class="text-reg card-title">Shadow and Bone</h6>
-                                            <p class="text-xs card-subtitle">Leigh Bardugo</p>
+                                    <div className="card-body px-0-75">
+                                        <div className="card-description my-0-25">
+                                            <h6 className="text-reg card-title">Shadow and Bone</h6>
+                                            <p className="text-xs card-subtitle">Leigh Bardugo</p>
                                         </div>          
                                     </div>
                                 </div>
@@ -124,10 +124,10 @@ const LandingPage = () => {
                         </section>
                         
                     </article>
-                    <a href="/pages/products/" class="btn text-center mx-auto mt-3 py-0-5 px-0-75">
+                    <a href="/pages/products/" className="btn text-center mx-auto mt-3 py-0-5 px-0-75">
                         Discover more books! 
-                        <span class="icon">
-                            <i class="fas fa-chevron-right"></i>
+                        <span className="icon">
+                            <i className="fas fa-chevron-right"></i>
                         </span>
                     </a>
                 </section>
