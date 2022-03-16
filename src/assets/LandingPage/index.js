42b259ab140e141dfca46612cd02910a80873e78
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-16 16:46:10 +0530

    feat: landing page images URL

---
+++ src/assets/LandingPage/index.js
@@ -0,0 +1,18 @@
+export const landingPageImages = {
+    genres: {
+        classics: 'https://github.com/bharati-21/bookery/blob/7e9cd40067b78bf87a44450a4fed898fe3990705/assets/images/home-page/classics.jpg?raw=true',
+        fantasy: 'https://github.com/bharati-21/bookery/blob/7e9cd40067b78bf87a44450a4fed898fe3990705/assets/images/home-page/fantasy.jpg?raw=true',
+        fiction: 'https://github.com/bharati-21/bookery/blob/7e9cd40067b78bf87a44450a4fed898fe3990705/assets/images/home-page/fiction.jpg?raw=true',
+        mystery: 'https://github.com/bharati-21/bookery/blob/7e9cd40067b78bf87a44450a4fed898fe3990705/assets/images/home-page/mystery.jpg?raw=true',
+        nonFiction: 'https://github.com/bharati-21/bookery/blob/7e9cd40067b78bf87a44450a4fed898fe3990705/assets/images/home-page/non-fiction.jpg?raw=true',
+        poem: 'https://github.com/bharati-21/bookery/blob/7e9cd40067b78bf87a44450a4fed898fe3990705/assets/images/home-page/poem.jpg?raw=true',
+        romance: 'https://github.com/bharati-21/bookery/blob/7e9cd40067b78bf87a44450a4fed898fe3990705/assets/images/home-page/romance.jpg?raw=true',
+        thriller: 'https://github.com/bharati-21/bookery/blob/7e9cd40067b78bf87a44450a4fed898fe3990705/assets/images/home-page/thriller.jpg?raw=true'
+    },
+    topPicks: {
+        aManCalledOve: 'https://github.com/bharati-21/bookery/blob/7e9cd40067b78bf87a44450a4fed898fe3990705/assets/images/products-page/Man-Called-Ove-Fredrick-Backman.jpg?raw=true',
+        shadowAndBone: 'https://github.com/bharati-21/bookery/blob/7e9cd40067b78bf87a44450a4fed898fe3990705/assets/images/products-page/Shadow-And-Bone-Leigh-Bardugo.jpg?raw=true',
+        atomicHabits: 'https://github.com/bharati-21/bookery/blob/7e9cd40067b78bf87a44450a4fed898fe3990705/assets/images/products-page/Atomic-habits-james-clear.jpg?raw=true'
+    }
+
+} 
