8b175dcd24abec53012f9dfb310654462c8f4622
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-18 11:07:25 +0530

    feat: helper util to fetch products

---
+++ src/utils/fetchProducts.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+export const fetchProducts = async (productDispatch) => {
+    try {
+        const { data, status } = await axios.get('/api/products');
+        if(status >= 200 && status < 300) {
+            productDispatch({
+                type: 'INIT_PRODUCTS_SUCCESS', 
+                payload: {
+                    products: data,
+                    error: null,
+                    loading: false
+                }
+            });
+            return;
+        }
+        productDispatch({
+            type: 'INIT_PRODUCTS_FAILURE', 
+            payload: {
+                products: [],
+                error: 'Books could not be fetched. Try again after sometime',
+                loading: false,
+            }
+        });
+    }
+    catch(error) {
+        productDispatch({
+            type: 'INIT_PRODUCTS_FAILURE', 
+            payload: {
+                products: [],
+                error: 'Books could not be fetched. Try again after sometime',
+                loading: false,
+            }
+        });
+    }
+}
