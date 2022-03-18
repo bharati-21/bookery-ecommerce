f10d77f772430bd7ac4ca5ce73d9476d20d55066
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-18 17:52:51 +0530

    feat: update items with dynamic product values

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,45 +1,85 @@
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
-import { landingPageImages } from '../assets/LandingPage/';
+const ProductItem = ({ product: 
+        {
+            author,
+            bookType,
+            coverImg,
+            discountPercent,
+            genres,
+            _id,
+            offers,
+            originalPrice,
+            sellingPrice,
+            title,
+            totalRatings,
+            totalStars
+        } 
+    }) => {
 
+    const genreMapping = genres.map(genre => 
+        <li key={`${_id}-${genre}`} className="badge text-xs badge-secondary p-0-25 px-0-5 genre-item">
+            {genre} 
+        </li>
+    );
 
-const ProductItem = () => {
-    const { topPicks :{ shadowAndBone }} = landingPageImages;
+    const productBadge = bookType === 'Hardcover' ?
+       <span className="badge badge-secondary mx-0-25 my-0-75 text-reg px-0-5">
+            {bookType}
+        </span>
+        :
+        offers.bestSeller ? 
+            <span className="badge badge-secondary mx-0-25 my-0-75 text-reg  px-0-75">
+                Best Seller
+            </span>
+        : 
+        offers.newArrival ?
+            <span className="badge badge-error mx-0-25 my-0-75 text-reg  px-0-75">
+                New Arrival
+            </span>:
+    null;
+        
     return (
-        <div className="product-card card card-vertical card-wt-dismiss">
+        <div className="product-card card card-vertical card-wt-dismiss card-wt-badge" id={_id}>
+            {productBadge ?? productBadge}
             <button className="btn btn-primary btn-icon btn-dismiss btn-card-wishlist m-0-5 flex--col flex-align-center flex-justify-center">
                 <span className="icon">
                     <FavoriteBorderIcon  />
                 </span>
             </button>
             <div className="card-header p-1">
-                <img src={shadowAndBone} alt="Shadow and Bone by Leigh Bardugo" />
+                <img src={coverImg} alt={title} />
             </div>
             <div className="card-body px-0-75">
                 <div className="card-description my-0-25">
-                    <h6 className="text-reg card-title">
-                        Shadow and Bone
+                    <h6 className="text-lg card-title">
+                        {title}
                     </h6>
-                    <p className="text-xs card-subtitle">
-                        Leigh Bardugo
+                    <p className="mt-0-25 text-sm card-subtitle">
+                        {author}
                     </p>
                 </div> 
-                <div className="card-content my-0-5">
+                <div className="card-content my-1">
                     <div className="card-price flex-row flex-align-start flex-justify-between">
-                        <div className="discounted-pric flex-col">
+                        <div className="discounted-price flex-col">
                             <p className="price-discounted">
-                                ₹299.00
+                                {sellingPrice}
                             </p>
                             <span className="success-color percentage-discount">
-                                Save 40%
+                                {discountPercent}
                             </span> 
                         </div>
                         <p className="price-original">
                             <span className="text-linethrough error-color">
-                                ₹499.00
+                                {originalPrice}
                             </span>   
                         </p>
+                    </div>
+                    <div className="card-genres">
+                        <ul className="list list-inline list-style-none mt-1-5 flex-row flex-wrap flex-justify-start">
+                            {genreMapping}
+                        </ul>
                     </div>
                 </div>            
             </div>
