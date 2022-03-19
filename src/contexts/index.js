87a83de27a1ee3d159b0d94ff0773a9231b4030a
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-19 19:23:05 +0530

    feat: export filter, provider context from 1 file

---
+++ src/contexts/index.js
@@ -0,0 +1,4 @@
+import { useProduct, ProductProvider } from "./product-context";
+import { useFilter, FilterProvider } from "./filter-context";
+
+export { useProduct, useFilter, ProductProvider, FilterProvider };
