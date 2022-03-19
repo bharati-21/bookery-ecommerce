b932a21ffd5bba7ee69e5dd27e3977c965d96e6a
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-19 19:22:07 +0530

    feat: initial filter reducer function

---
+++ src/reducers/filterReducer.js
@@ -0,0 +1,14 @@
+const filterActions = {
+    FILTER_BY_GENRE: 'FILTER_BY_GENRE',
+    FILTER_BY_PRICE: 'FILTER_BY_PRICE',
+    FILTER_BY_RATINGS: 'FILTER_BY_RATINGS',
+    FILTER_BY_BOOK_TYPE: 'FILTER_BY_BOOK_TYPE',
+    SORT_BY_PRICE: 'SORT_BY_PRICE',
+    SORT_BY_RATINGS: 'SORT_BY_RATINGS'
+};
+
+const filterReducerFunction = (prevFilterState, {filterAction, filterData }) => {
+    
+}
+
+export { filterReducerFunction };
