25827a3b3a5fdc970bbbf5ec4c61c4543be3209a
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 17:13:12 +0530

    feat: filter actions and state updates

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -7,8 +7,31 @@ const filterActions = {
     SORT_BY_RATINGS: 'SORT_BY_RATINGS'
 };
 
-const filterReducerFunction = (prevFilterState, {filterAction, filterData }) => {
-    
+const filterReducerFunction = (prevFilterState, {filterType, filterPayload }) => {
+    switch(filterType) {
+        case filterActions.FILTER_BY_GENRE:
+            return {...prevFilterState, 
+                genres: {
+                    ...prevFilterState.genres,
+                    [filterPayload]: !prevFilterState.genres[filterPayload]
+                }
+            };
+
+        case filterActions.FILTER_BY_BOOK_TYPE:
+            return {...prevFilterState, 
+                bookType: {
+                    ...prevFilterState.bookType,
+                    [filterPayload]: !prevFilterState.bookType[filterPayload]
+                }
+            };
+
+        case filterActions.FILTER_BY_RATINGS: 
+            return {...prevFilterState, ratingsMoreThan: filterPayload};
+
+        case filterActions.FILTER_BY_PRICE: 
+            return {...prevFilterState, price: filterPayload};
+
+    }
 }
 
 export { filterReducerFunction };
