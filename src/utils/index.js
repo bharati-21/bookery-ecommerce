15de19f6bc4d646b2636a8ddfa4ec0dc5b29d84c
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 20:54:08 +0530

    feat: add sort util function to common util file

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,7 @@ import { filterByGenres } from './filterByGenres';
 import { filterByPrice } from './filterByPrice';
 import { filterByRatings } from './filterByRatings';
 import { filterByBookTypes } from './filterByBookTypes';
+import { sortBooks } from './sortBooks';
 
 
-export { fetchProducts, filterByGenres, filterByPrice, filterByRatings, filterByBookTypes };
+export { fetchProducts, filterByGenres, filterByPrice, filterByRatings, filterByBookTypes, sortBooks };
