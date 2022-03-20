64cce75a89cc81a36dbdedfdcfb27285865c6510
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 17:50:19 +0530

    feat: logic to filter books by price

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 import { fetchProducts } from "./fetchProducts";
 import { filterByGenres } from './filterByGenres';
+import { filterByPrice } from './filterByPrice';
 
 
-export { fetchProducts, filterByGenres };
+export { fetchProducts, filterByGenres, filterByPrice };
