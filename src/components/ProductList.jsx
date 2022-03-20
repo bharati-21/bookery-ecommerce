8993e62af7e46e9200ca12b3d448517949f415b2
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 20:54:45 +0530

    feat: render sorted book list

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,23 +1,25 @@
 import { useFilter } from '../contexts';
-import { filterByGenres, filterByPrice, filterByRatings, filterByBookTypes } from '../utils/';
+import { filterByGenres, filterByPrice, filterByRatings, filterByBookTypes, sortBooks } from '../utils/';
 import { ProductItem } from './ProductItem';
 
 const ProductList = ({ products }) => {
 
-    const { filterState } = useFilter();
+    const { filterState : { genres, price, ratingsMoreThan, bookType, sortBy } } = useFilter();
 
-    const productsFilteredByGenre = filterByGenres(products, filterState.genres);
-    const productsFilteredByPrice = filterByPrice(productsFilteredByGenre, filterState.price);
-    const productsFilteredByRatings = filterByRatings(productsFilteredByPrice, filterState.ratingsMoreThan);
-    const productsFilteredByBookTypes = filterByBookTypes(productsFilteredByRatings, filterState.bookType);
+    const booksFilteredByGenre = filterByGenres(products, genres);
+    const booksFilteredByPrice = filterByPrice(booksFilteredByGenre, price);
+    const booksFilteredByRatings = filterByRatings(booksFilteredByPrice, ratingsMoreThan);
+    const booksFilteredByBookTypes = filterByBookTypes(booksFilteredByRatings, bookType);
+
+    const booksSortedByOptions = sortBooks(booksFilteredByBookTypes, sortBy);
 
     return (
         <section className="product-catalog">
-            <p className="text-reg mb-1">Number of Books: <span className="primary-color">{productsFilteredByBookTypes.length}</span></p>
+            <p className="text-reg mb-1">Number of Books: <span className="primary-color">{booksSortedByOptions.length}</span></p>
             <article className="products-container grid grid-autofit">
                 {
-                    productsFilteredByBookTypes.length > 0 ?
-                    productsFilteredByBookTypes.map(product => <ProductItem product={product} key={product._id} />)
+                    booksSortedByOptions.length > 0 ?
+                    booksSortedByOptions.map(product => <ProductItem product={product} key={product._id} />)
                         :
                     <h2 className="my-1 text-center error-color">No Books Found!</h2>
                 }
