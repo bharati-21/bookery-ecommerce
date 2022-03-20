fc4b051b435b8c857c75d5b0811852ac0c93d6bb
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 21:20:09 +0530

    refactor: remove invalid comment

--- src/utils/sortBooks.js
+++ src/utils/sortBooks.js
@@ -1,6 +1,5 @@
 const sortBooks = (filteredBooks, sortByOption) => {
     
-    // Are none of the book types checked? If yes, then just return the ratings filtered books
     switch(sortByOption) {
         case 'PRICE_LOW_TO_HIGH':
             return [...filteredBooks].sort((filteredBookA, filteredBookB) => filteredBookA.sellingPrice - filteredBookB.sellingPrice);
