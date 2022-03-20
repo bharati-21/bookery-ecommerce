ff3b40313153b91c661e0152cba4d82244efbb1b
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 17:14:09 +0530

    feat: filter mappings, onchange events

---
+++ src/components/FilterOptions/GenreFilters/GenreFilterOption.jsx
@@ -0,0 +1,28 @@
+import { useFilter } from '../../../contexts';
+
+const GenreFilterOption = ({ categoryName }) => {
+
+    const {filterState : { genres }, filterDispatch} = useFilter();
+
+    const isGenreChecked = genres[categoryName];   
+
+    const handleGenreChange = (event) => {
+        filterDispatch({
+            filterType: 'FILTER_BY_GENRE',
+            filterPayload: event.target.value
+        });
+    }
+
+    return (
+        <li className="list-item mt-0-25 p-0-25">
+            <label htmlFor={`genre-${categoryName}`}  className="text-sm genre-label flex-row flex-align-center">
+                <input type="checkbox" className="input-checkbox" value={categoryName} id={`genre-${categoryName}`} onChange={handleGenreChange} checked={isGenreChecked}/>
+                <span className="genre-text">
+                    {categoryName}
+                </span>
+            </label>
+        </li>
+    );
+}
+
+export { GenreFilterOption };
