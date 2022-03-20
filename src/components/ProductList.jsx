b173913620d313ebabd37a58f950662df8eb0e14
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 18:36:00 +0530

    feat: render rating filtered books

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,5 @@
 import { useFilter } from '../contexts';
-import { filterByGenres, filterByPrice } from '../utils/';
+import { filterByGenres, filterByPrice, filterByRatings } from '../utils/';
 import { ProductItem } from './ProductItem';
 
 const ProductList = ({ products }) => {
@@ -8,13 +8,17 @@ const ProductList = ({ products }) => {
 
     const productsFilteredByGenre = filterByGenres(products, filterState.genres);
     const productsFilteredByPrice = filterByPrice(productsFilteredByGenre, filterState.price);
+    const productsFilteredByRatings = filterByRatings(productsFilteredByPrice, filterState.ratingsMoreThan)
 
     return (
         <section className="product-catalog">
-            <p className="text-reg mb-1">Number of Books: <span className="primary-color">{productsFilteredByPrice.length}</span></p>
+            <p className="text-reg mb-1">Number of Books: <span className="primary-color">{productsFilteredByRatings.length}</span></p>
             <article className="products-container grid grid-autofit">
                 {
-                    productsFilteredByPrice.map(product => <ProductItem product={product} key={product._id} />)
+                    productsFilteredByRatings.length > 0 ?
+                        productsFilteredByRatings.map(product => <ProductItem product={product} key={product._id} />)
+                        :
+                    <h2 className="my-1 text-center error-color">No Books Found!</h2>
                 }
             </article>
         </section>
