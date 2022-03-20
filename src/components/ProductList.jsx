0b38628a74bd6e7e4d781c7e6bfa92677fb2fd9e
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 17:22:50 +0530

    feat: get genre filtered books and render

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,18 @@
+import { useFilter } from '../contexts';
+import { filterByGenres } from '../utils/';
 import { ProductItem } from './ProductItem';
 
 const ProductList = ({ products }) => {
+
+    const { filterState } = useFilter();
+
+    const productsFilteredByGenre = filterByGenres(products, filterState.genres);
+
     return (
         <section className="product-catalog">
             <article className="products-container grid grid-autofit">
                 {
-                    products.map(product => <ProductItem product={product} key={product._id} />)
+                    productsFilteredByGenre.map(product => <ProductItem product={product} key={product._id} />)
                 }
             </article>
         </section>
