f2981c6a090600c3b7d2d1c2f374ce62cb700282
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 19:03:01 +0530

    feat: logic to filter books by book type

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,7 @@ import { fetchProducts } from "./fetchProducts";
 import { filterByGenres } from './filterByGenres';
 import { filterByPrice } from './filterByPrice';
 import { filterByRatings } from './filterByRatings';
+import { filterByBookTypes } from './filterByBookTypes';
 
 
-export { fetchProducts, filterByGenres, filterByPrice, filterByRatings };
+export { fetchProducts, filterByGenres, filterByPrice, filterByRatings, filterByBookTypes };
