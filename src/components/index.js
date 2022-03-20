1d4250ba232242a7f7aeb9ff703a4a97210d7f21
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 17:15:28 +0530

    feat: update component index with new components

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,16 @@ import { ProductItem } from './ProductItem';
 import { ProductList } from './ProductList';
 import { SortingOptions } from './SortingOptions';
 
+import { BookTypeFilterOption } from './FilterOptions/BookTypeFilters/BookTypeFilterOption'
+import { BookTypeFilters } from './FilterOptions/BookTypeFilters/BookTypeFilters';
 
-export { Filters, Footer, Header, Navbar, ProductList, ProductItem, SortingOptions};
+import { RatingFilterOption } from './FilterOptions/RatingFilters/RatingFilterOption';
+import { RatingFilters } from './FilterOptions/RatingFilters/RatingFilters';
+
+import { GenreFilterOption } from './FilterOptions/GenreFilters/GenreFilterOption';
+import { GenreFilters } from './FilterOptions/GenreFilters/GenreFilters';
+
+import { PriceFilters } from './FilterOptions/PriceFilters/PriceFilters';
+import { ClearFilters } from './FilterOptions/ClearFilters'
+
+export { Filters, Footer, Header, Navbar, ProductList, ProductItem, SortingOptions, GenreFilterOption, BookTypeFilterOption, RatingFilterOption, GenreFilters, PriceFilters, RatingFilters, BookTypeFilters, ClearFilters };
