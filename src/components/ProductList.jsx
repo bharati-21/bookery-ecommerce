69d8c3ccc61a525d6df4a820d174b6ccb4da1f92
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 19:03:18 +0530

    feat: render books filtered by book types

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,5 @@
 import { useFilter } from '../contexts';
-import { filterByGenres, filterByPrice, filterByRatings } from '../utils/';
+import { filterByGenres, filterByPrice, filterByRatings, filterByBookTypes } from '../utils/';
 import { ProductItem } from './ProductItem';
 
 const ProductList = ({ products }) => {
@@ -8,15 +8,16 @@ const ProductList = ({ products }) => {
 
     const productsFilteredByGenre = filterByGenres(products, filterState.genres);
     const productsFilteredByPrice = filterByPrice(productsFilteredByGenre, filterState.price);
-    const productsFilteredByRatings = filterByRatings(productsFilteredByPrice, filterState.ratingsMoreThan)
+    const productsFilteredByRatings = filterByRatings(productsFilteredByPrice, filterState.ratingsMoreThan);
+    const productsFilteredByBookTypes = filterByBookTypes(productsFilteredByRatings, filterState.bookType);
 
     return (
         <section className="product-catalog">
-            <p className="text-reg mb-1">Number of Books: <span className="primary-color">{productsFilteredByRatings.length}</span></p>
+            <p className="text-reg mb-1">Number of Books: <span className="primary-color">{productsFilteredByBookTypes.length}</span></p>
             <article className="products-container grid grid-autofit">
                 {
-                    productsFilteredByRatings.length > 0 ?
-                        productsFilteredByRatings.map(product => <ProductItem product={product} key={product._id} />)
+                    productsFilteredByBookTypes.length > 0 ?
+                    productsFilteredByBookTypes.map(product => <ProductItem product={product} key={product._id} />)
                         :
                     <h2 className="my-1 text-center error-color">No Books Found!</h2>
                 }
