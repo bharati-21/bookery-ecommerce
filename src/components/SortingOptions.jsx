30d66dc7cbe0be487a914c29debf7857d9f56481
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-20 20:52:32 +0530

    feat: map through sorting options and render

--- src/components/SortingOptions.jsx
+++ src/components/SortingOptions.jsx
@@ -1,33 +1,51 @@
+import { useFilter } from "../contexts";
+import { SortingOptionItem } from './SortingOptionItem';
+import { v4 as uuid } from 'uuid';
+
 const SortingOptions = () => {
-  return (
-    <aside className='sorting-container flex-col align-center justify-start'>
-        <ul className="list list-inline list-sorting-options flex-row align-center justify-start flex-wrap">
-            <p className="list-head primary-color">
-                Sort By
-            </p>
-            <li className="sorting-option border-bottom">
-                <button className="btn btn-link">
-                    Price- High to Low
-                </button>
-            </li>
-            <li className="sorting-option border-bottom">
-                <button className="btn btn-link">
-                    Price- Low to High
-                </button>
-            </li>
-            <li className="sorting-option border-bottom">
-                <button className="btn btn-link">
-                    Rating- Hight to Low
-                </button>
-            </li>
-            <li className="sorting-option border-bottom">
-                <button className="btn btn-link">
-                    Rating- Low to High
-                </button>
-            </li>
-        </ul>
-    </aside>
-  )
+
+    const { filterDispatch, filterState: { sortBy } } = useFilter(); 
+
+    const handleChangeSortingOption = event => {
+        const { value } = event.target;
+        filterDispatch({filterType: 'SORT_BY', filterPayload: value});
+    }
+
+    const sortingOptionList = [
+        {
+            id: uuid(),
+            option: 'Price- High to Low',
+            value: 'PRICE_HIGH_TO_LOW'
+        },
+        {
+            id: uuid(),
+            option: 'Price- Low to High',
+            value: 'PRICE_LOW_TO-HIGH'
+        },
+        {
+            id: uuid(),
+            option: 'Ratings- High to Low',
+            value: 'RATINGS_HIGH_TO_LOW'
+        },
+        {
+            id: uuid(),
+            option: 'Ratings- Low to High',
+            value: 'RATINGS_LOW_TO_HIGH'
+        },
+    ]
+
+    const sortingOptionsMapping = sortingOptionList.map(({ id, option, value }) => <SortingOptionItem key={id} props={{ option, handleChangeSortingOption, sortBy, value }}/>)
+
+    return (
+        <aside className='sorting-container flex-col align-center justify-start'>
+            <ul className="list list-inline list-sorting-options flex-row align-center justify-start flex-wrap">
+                <li className="list-head primary-color">
+                    Sort By
+                </li>
+                { sortingOptionsMapping }
+            </ul>
+        </aside>
+    )
 }
 
 export { SortingOptions };
