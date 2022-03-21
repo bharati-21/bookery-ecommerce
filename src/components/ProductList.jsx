da883af61b8d78922164fdb9347c0d01cbfc3fa5
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-21 09:10:13 +0530

    refactor: compose() call, edit product to book

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,25 +1,16 @@
-import { useFilter } from 'contexts/';
-import { filterByGenres, filterByPrice, filterByRatings, filterByBookTypes, sortBooks } from 'utils/';
+import { compose } from 'utils/';
 import { ProductItem } from './ProductItem';
 
 const ProductList = ({ products }) => {
-
-    const { filterState : { genres, price, ratingsMoreThan, bookType, sortBy } } = useFilter();
-
-    const booksFilteredByGenre = filterByGenres(products, genres);
-    const booksFilteredByPrice = filterByPrice(booksFilteredByGenre, price);
-    const booksFilteredByRatings = filterByRatings(booksFilteredByPrice, ratingsMoreThan);
-    const booksFilteredByBookTypes = filterByBookTypes(booksFilteredByRatings, bookType);
-
-    const booksSortedByOptions = sortBooks(booksFilteredByBookTypes, sortBy);
+    const sortedFilteredBooks = compose(products);  
 
     return (
         <section className="product-catalog">
-            <p className="text-reg mb-1">Number of Books: <span className="primary-color">{booksSortedByOptions.length}</span></p>
+            <p className="text-reg mb-1">Number of Books: <span className="primary-color">{sortedFilteredBooks.length}</span></p>
             <article className="products-container grid grid-autofit">
                 {
-                    booksSortedByOptions.length > 0 ?
-                    booksSortedByOptions.map(product => <ProductItem product={product} key={product._id} />)
+                    sortedFilteredBooks.length > 0 ?
+                    sortedFilteredBooks.map(book => <ProductItem book={book} key={book._id} />)
                         :
                     <h2 className="my-1 text-center error-color">No Books Found!</h2>
                 }
