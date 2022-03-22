c7482f33162194d84f2babd719223de48bd03e60
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-22 19:09:21 +0530

    feat: sign up form data, password visibility

--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -1,9 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 const Signup = () => {
+
+    const [formData, setFormData] = useState(
+        {
+            name: '',
+            email: '',
+            password: '',
+        }
+    );
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleFormDataChange = event => {
+        const { name, value } = event.target;
+        setFormData(prevFormData => ({...prevFormData, [name]: value}));
+    }
+
+    const showPasswordIcon = showPassword ? <VisibilityOffIcon /> : <VisibilityIcon /> 
+
+    const handleFormSubmit = event => {
+        console.log('Login Form Submit Handler')
+    }
+
+    const { name, email, password } = formData;
+    const handleChangePasswordVisibility = () => setShowPassword(prevShowPassword => !prevShowPassword);
+
     return (
         <section className="auth-main flex-col flex-align-center flex-justify-center mx-auto py-2 px-3">
             <div className="auth-wrapper">
@@ -11,18 +36,18 @@ const Signup = () => {
                     <h3 className="text-center text-uppercase auth-head mb-2">
                         Sign Up
                     </h3>
-                    <form className="auth-form px-1">
+                    <form method="POST" className="auth-form px-1" onSubmit={handleFormSubmit}>
                         <div className="input-group input-default mt-1-5 mx-auto">
                             <label className="text-label text-reg flex-col mx-auto" htmlFor="input-signup-fname">
                                 Name
-                                <input type="text" id="input-signup-fname" className="input-text text-sm px-0-75 py-0-5 mt-0-25" placeholder="Jane Doe" required />
+                                <input type="text" id="input-signup-fname" className="input-text text-sm px-0-75 py-0-5 mt-0-25" placeholder="Jane Doe" name="name" onChange={handleFormDataChange} value={name} required />
                             </label>
                             <span className="text-message mt-0-5"></span>
                         </div>
                         <div className="input-group input-default mt-1-5 mx-auto">
                             <label className="text-label text-reg flex-col mx-auto" htmlFor="input-login-email">
                                 Email
-                                <input type="email" id="input-login-email" className="input-text text-sm px-0-75 py-0-5 mt-0-25" placeholder="janedoe@example.com" required/>
+                                <input type="email" id="input-login-email" className="input-text text-sm px-0-75 py-0-5 mt-0-25" placeholder="janedoe@example.com" name="email" onChange={handleFormDataChange} value={email} required/>
                             </label>
                             <span className="text-message mt-0-5"></span>
                         </div>
@@ -30,10 +55,10 @@ const Signup = () => {
                             <label className="text-label text-reg flex-col mx-auto text-sm" htmlFor="input-psd">
                                 Password
                                 <span className="password-input-toggler">
-                                    <input type="password" id="input-psd" className="input-text px-0-75 py-0-5 mt-0-25" placeholder="********" required />
-                                    <button className="btn btn-icon icon-show-psd">
-                                        <span className="mui-icon">
-                                            <VisibilityIcon />
+                                    <input type={`${showPassword ? 'text' : 'password'}`} id="input-psd" className="input-text px-0-75 py-0-5 mt-0-25" placeholder="********" autoComplete='off' name="password" onChange={handleFormDataChange} value={password} required />
+                                    <button type="button" className="btn btn-icon icon-show-psd" onClick={handleChangePasswordVisibility}>
+                                        <span className="icon mui-icon">
+                                            {showPasswordIcon}
                                         </span>
                                     </button>
                                 </span>  
@@ -54,7 +79,7 @@ const Signup = () => {
                             <input type="submit" value="Sign Up" className="btn btn-primary px-0-75 py-0-25 btn-full-width text-reg" />
                             <Link to="/login" className="btn btn-link btn-primary mt-0-75">
                                 Already have an account?
-                                Login <span className="icon-chevron-right">
+                                Login <span className="icon icon-chevron-right">
                                     <ChevronRightIcon />
                                 </span>
                             </Link>
