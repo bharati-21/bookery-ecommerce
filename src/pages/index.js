85ee5328c1862d96b975a7cbd850bb916709be20
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-22 11:16:43 +0530

    refactor: edit page imports in common export file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import { LandingPage } from "./LandingPage";
-import { Products } from "./Products";
-import { Cart } from "./Cart";
-import { WishList } from "./WishList";
-import { Signup } from "./Signup";
-import { Login } from "./Login";
-import { Logout } from "./Logout";
+import { LandingPage } from "./HomePage/LandingPage";
+import { Products } from "./Products/Products";
+import { Cart } from "./Cart/Cart";
+import { WishList } from "./WishList/WishList";
+import { Signup } from "./Auth/Signup";
+import { Login } from "./Auth/Login";
+import { Logout } from "./Auth/Logout";
 
 export { LandingPage, Products, Cart, WishList, Signup, Login, Logout };
