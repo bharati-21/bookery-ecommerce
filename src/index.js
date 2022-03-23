92555d9afbae8f56277659f2ce64e3a078f3e24b
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-23 13:55:05 +0530

    feat: add Toast container, auth provider to root

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import App from "./App";
 import { makeServer } from "./server";
 
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ProductProvider, FilterProvider } from "contexts/";
+import { ToastContainer } from 'react-toastify';
+
+import { ProductProvider, FilterProvider, AuthProvider } from "contexts/";
 
 // Call make Server
 makeServer();
@@ -12,11 +14,23 @@ makeServer();
 ReactDOM.render(
     <React.StrictMode>
         <Router>
-            <ProductProvider>
-                <FilterProvider>
-                    <App />
-                </FilterProvider>
-            </ProductProvider>
+            <AuthProvider>
+                <ProductProvider>
+                    <FilterProvider>
+                        <ToastContainer 
+                            position="bottom-left"
+                            autoClose={2000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            draggable
+                            pauseOnHover
+                        />
+                        <App />
+                    </FilterProvider>
+                </ProductProvider>
+            </AuthProvider>
         </Router>
     </React.StrictMode>,
     document.getElementById("root")
