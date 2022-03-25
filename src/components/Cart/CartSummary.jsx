d92b4d198a79b7011e3fb50c1034bf431ba356d6
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-25 23:46:16 +0530

    feat: display total SP, CP, Delivery, discount price

--- src/components/Cart/CartSummary.jsx
+++ src/components/Cart/CartSummary.jsx
@@ -1,23 +1,43 @@
-const CartSummary = ({ totalNumItems }) => {
+import { getCartItemsData } from "utils/";
+
+const CartSummary = ({ cartItems }) => {
+
+    const { cartItemsTotalPrice, cartItemsTotalSavingPrice, cartItemsTotalOriginalPrice } = getCartItemsData(cartItems);
+
+    const numCartItems = cartItems.length;
+
+    const deliveryCharges = cartItemsTotalPrice < 500 ? (
+        <p className="text-reg item-content">₹ 49</p>
+    ) : (
+        <p className="text-reg item-content">FREE</p>
+    );
+    
     return (
         <section className="checkout-container p-2 flex-col flex-align-center flex-justify-start">
             <h4 className="section-head pb-1 text-center">
-                Price Details
+                Price Details 
+                <p className="text-reg">
+                ({numCartItems} items)
+                </p>
             </h4>
             <article className="payment-details">
                 <div className="flex-col py-1-5">
-                    <div className="flex-row total-price flex-justify-between flex-align-center py-0-25">
-                        <p className="text-reg item-head">Price ({totalNumItems} items)</p>
-                        <p className="text-reg item-content">₹ 798</p>
+                    <div className="flex-row total-original-price flex-justify-between flex-align-center py-0-25">
+                        <p className="text-reg item-head">Total Price</p>
+                        <p className="text-reg item-content">₹ {cartItemsTotalOriginalPrice}</p>
+                    </div>
+                    <div className="flex-row total-saving-price flex-justify-between flex-align-center py-0-25">
+                        <p className="text-reg item-head">Discount</p>
+                        <p className="text-reg item-content">- ₹ {cartItemsTotalSavingPrice}</p>
                     </div>
                     <div className="flex-row delivery-charges flex-justify-between flex-align-center py-0-25">
                         <p className="text-reg item-head">Delivery Charges</p>
-                        <p className="text-reg item-content">- ₹ 49</p>
+                        {deliveryCharges}
                     </div>
                 </div>
                 <div className="total-charges pt-1 flex-row flex-justify-between flex-align-center">
                     <p className="text-lg item-head">Subtotal</p>
-                    <p className="text-lg item-content">₹ 647</p>
+                    <p className="text-lg item-content">₹ {cartItemsTotalPrice}</p>
                 </div>
                 <div className="pb-0-5 pt-1">
                     <label htmlFor="checkbox-gift">
@@ -25,6 +45,7 @@ const CartSummary = ({ totalNumItems }) => {
                         This order contains a gift.
                     </label>
                 </div>
+                
                 <button className="btn btn-full-width mt-1 text-lg py-0-25 px-0-5">
                     Place order 
                 </button>                        
