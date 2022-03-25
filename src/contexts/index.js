ad917d6bf802f06441ac94e0e6e269623d031268
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-25 18:35:59 +0530

    feat: export cart context reducer from common file

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,4 @@
 export { useProduct, ProductProvider } from "./product-context";
 export { useFilter, FilterProvider } from "./filter-context";
 export { useAuth, AuthProvider } from "./auth-context";
+export { useCart, CartProvider } from "./cart-context"
