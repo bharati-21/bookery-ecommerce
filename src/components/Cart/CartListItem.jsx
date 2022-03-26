91fdcb97034c1c3f72391cf4d599e51a3c6d11cd
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-26 14:53:50 +0530

    refactor: update and delete cart item n/w calls

--- src/components/Cart/CartListItem.jsx
+++ src/components/Cart/CartListItem.jsx
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+import { useAuth, useCart } from "contexts/";
+import { updateProductInCart, deleteProductInCart } from 'utils/'
+import { useToast } from "custom-hooks/useToast";
+
 const CartListItem = ({ cartItem }) => {
+    const [isOngoingNetworkCall, setIsOngoingNetworkCall] = useState(false); 
     const {
         author,
         bookType,
@@ -7,9 +13,69 @@ const CartListItem = ({ cartItem }) => {
         _id,
         sellingPrice,
         title,
+        qty
     } = cartItem;
  
     const localeSellingPrice = sellingPrice.toLocaleString("en-IN", {minimumFractionDigits: 2, maximumFractionDigits: 2});
+
+    const { authState: { token } } = useAuth();
+    const { showToast } = useToast();
+    const { cartDispatch } = useCart();
+
+    const handleCartQuantityButtonClick = async event => {
+        const { value } = event.target;
+        let operation = '';
+        switch(value) {
+            case 'DECREASE_QUANTITY':
+                operation = 'decrement';
+                break;
+            case 'INCREASE_QUANTITY':
+                operation = 'increment'
+                break;
+            default: 
+                operation = '';
+                break;
+        }
+
+        if(qty >= 3 && operation === 'increment') {
+            showToast('We are sorry! Only 3 units allowed in each order!', 'error');
+            return;
+        }
+
+        try {
+            setIsOngoingNetworkCall(true);
+            const productUpdatedInCart = await updateProductInCart(_id, token, operation);
+
+            if(productUpdatedInCart) {
+                showToast('Quantity updated successfully!', 'success');
+                cartDispatch({type: 'ADD_TO_CART', payload: {cartItems: productUpdatedInCart, error: false, loading: false}});
+            }
+            else {
+                showToast('Failed to update quantity. Please try again later.', 'error');
+            }
+            setIsOngoingNetworkCall(false);
+        }
+        catch(error) {
+            console.log('Something went really wrong!');
+            setIsOngoingNetworkCall(false);
+        }        
+    }
+
+    const handleRemoveFromCart = async () => {
+        try {
+            const productDeletedInCart = await deleteProductInCart(_id, token);
+            if(productDeletedInCart) {
+                showToast('Product deleted successfully!', 'success');
+                cartDispatch({type: 'ADD_TO_CART', payload: {cartItems: productDeletedInCart, error: false, loading: false}});
+            }
+            else {
+                showToast('Failed to delete product. Please try again later.', 'error');
+            }
+        }
+        catch(error) {
+            console.log('Something went really wrong!');
+        }
+    }
 
     return (
         <article className="card card-horizontal product-card cart-item flex-row flex-align-center flex-justify-center p-1" id={_id}>
@@ -34,17 +100,37 @@ const CartListItem = ({ cartItem }) => {
                         </p>    
                     </div>
                     <div className="card-quantity flex-row flex-align-center flex-justify-start">
-                        <button className="btn btn-quantity btn-decrease-quantity flex-col">-</button>
-                        <input type="number" name="cart-item-quantity" className="input-text input-quantity px-0-75 py-0-25 text-center flex-col" id="cart-item-quantity" min="1" />
-                        <button className="btn btn-quantity btn-increase-quantity">+</button> 
+                        <button 
+                            className={`btn btn-quantity btn-decrease-quantity flex-col ${qty <=1 || isOngoingNetworkCall? 'btn-disabled' : ''}`} 
+                            value="DECREASE_QUANTITY" 
+                            disabled={qty <= 1 || isOngoingNetworkCall} 
+                            onClick={handleCartQuantityButtonClick}>
+                                -
+                        </button>
+                        <input 
+                            type="number" 
+                            name="cart-item-quantity" 
+                            className="input-text input-quantity px-0-75 py-0-25 text-center flex-col" 
+                            id="cart-item-quantity" 
+                            value={qty} 
+                            min="1" 
+                            readOnly
+                        />
+                        <button 
+                            className={`btn btn-quantity btn-increase-quantity flex-col ${isOngoingNetworkCall ? 'btn-disabled' : ''}`}  
+                            value="INCREASE_QUANTITY" 
+                            disabled={isOngoingNetworkCall} 
+                            onClick={handleCartQuantityButtonClick}>
+                                +
+                        </button> 
                     </div>
                 </div>
             </div>
             <div className="card-footer flex-row flex-align-center flex-justify-content flex-wrap">
-                    <button className="btn btn-primary px-0-75 py-0-25 btn-full-width text-reg">
-                        Add to wishlist
+                    <button className={`btn btn-primary px-0-75 py-0-25 btn-full-width text-reg ${isOngoingNetworkCall ? 'btn-disabled' : ''}`}>
+                        Move to wishlist
                     </button>
-                    <button className="btn btn-primary btn-outline px-0-75 py-0-25 btn-full-width text-reg">
+                    <button className={`btn btn-primary btn-outline px-0-75 py-0-25 btn-full-width text-reg ${isOngoingNetworkCall ? 'btn-disabled' : ''}`} onClick={handleRemoveFromCart}>
                         Remove from cart
                     </button>
                 </div>
