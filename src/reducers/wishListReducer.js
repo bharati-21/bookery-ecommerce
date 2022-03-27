829226da410bdd13b1a229f8b15e8905592883a2
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-27 12:52:27 +0530

    feat: reducer for wishlist state

---
+++ src/reducers/wishListReducer.js
@@ -0,0 +1,28 @@
+const wishListActionTypes = {
+    INIT_WISHLIST_ITEMS: 'INIT_WISHLIST_ITEMS',
+    ADD_TO_WISHLIST_FAILURE: 'ADD_TO_WISHLIST_FAILURE',
+    ADD_TO_WISHLIST: 'ADD_TO_WISHLIST',
+};
+
+const wishListReducerFunction = (prevWishListState, {type, payload: {wishListItems, error, loading}}) => {
+    switch(type) {
+
+        case wishListActionTypes.INIT_WISHLIST_ITEMS :
+            return {...prevWishListState, wishListItems, error, loading};
+
+        case wishListActionTypes.ADD_TO_WISHLIST_FAILURE :
+            return {
+                ...prevWishListState, wishListItems: [],  error, loading
+            }
+
+        case wishListActionTypes.ADD_TO_WISHLIST: 
+            return {
+                ...prevWishListState, wishListItems, error, loading
+            }
+
+        default: 
+            return {...prevWishListState}
+    }
+}
+
+export { wishListReducerFunction };
