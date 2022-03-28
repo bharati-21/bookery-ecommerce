7ba071fe09001de3f7bcecd3179c2598e4550505
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-28 16:17:18 +0530

    feat: basic structure for Wishlist component

--- src/pages/WishList/WishList.jsx
+++ src/pages/WishList/WishList.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { WishListItem } from "components";
+import { useWishList } from "contexts";
+import './WishList.css';
+import { Link } from 'react-router-dom';
 
 const WishList = () => {
-  return (
-    <div className="page-wrapper flex-col flex-align-center flex-justify-center">This is WishList Page</div>
-  )
+    const { wishListState: { wishListItems, loading, error } } = useWishList();
+    return (
+        <main className="main wishlist-main my-2 mx-auto py-2 px-3">
+            {
+                loading ? <h1 className="loading text success-color my-2 text-center">Loading Wishlist Items...</h1>
+                :
+                error ? <h1 className="error text error-color my-2 text-center">{error}</h1>
+                :
+                <>
+                    {
+                        wishListItems.length ?
+                            <div className="wishlist-main-head">
+                                <h2 className="main-head text-left mb-2 py-0-25 text-center">
+                                    My Wishlist
+                                </h2>
+                                <section className="wishList-wrapper products-container grid grid-autofit">
+                                 {
+                                    wishListItems.map(wishListItem => <WishListItem key={wishListItem._id} wishListItem={wishListItem} />
+                                    )
+                                }
+                                </section>
+                            </div>
+                        :
+                        <>
+                            <div className="wishlist-main-head">
+                                <h2 className="main-head text-center">
+                                    Your Wishlist empty!
+                                </h2>
+                            </div>
+                        </>
+                    }
+                </>
+            }
+            <button className="btn btn-primary mt-2 px-0-75 py-0-25 mx-auto"><Link to="/products">
+            Shop for books!</Link></button>
+        </main>
+    )
 }
 
 export { WishList };
