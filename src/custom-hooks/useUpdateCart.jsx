d1e53bf4c752f708e4912e47e39e22c64ab1274b
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-28 16:27:07 +0530

    feat: custom hook with function to update cart qty

---
+++ src/custom-hooks/useUpdateCart.jsx
@@ -0,0 +1,33 @@
+import { useToast} from './useToast';
+import { useCart, useAuth } from 'contexts/';
+import { updateProductInCart } from 'utils';
+
+const useUpdateCart = () => {
+
+    const { showToast } = useToast();
+    const { cartDispatch } = useCart();
+    const { authState: { token } } = useAuth();
+
+    const callUpdateProductInCart = async (_id, operation, showToastAfterCall=true) => {
+        try {
+            const productUpdatedInCart = await updateProductInCart(_id, token, operation);
+            if(productUpdatedInCart) {
+                if(showToastAfterCall) showToast('Item quantity updated in cart!', 'success');
+                cartDispatch({type: 'ADD_TO_CART', payload: {cartItems: productUpdatedInCart, error: false, loading: false}});
+                return true;
+            }
+            else {
+                if(showToastAfterCall) ('Failed to update item quantity in cart. Please try again later.', 'error');
+                return false;
+            }
+        }
+        catch(error) {
+            console.log(error.message);
+            return false;
+        }
+    }
+
+    return { callUpdateProductInCart };
+}
+
+export { useUpdateCart };
