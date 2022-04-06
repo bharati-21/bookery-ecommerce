9779b33e1228d56f549114b9c56efa5daf7ec60c
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-06 19:02:39 +0530

    refactor: fetch products util

--- src/utils/fetchProducts.js
+++ src/utils/fetchProducts.js
@@ -1,35 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
+
 export const fetchProducts = async (productDispatch) => {
-    try {
-        const { data : {products}, status } = await axios.get('/api/products');
-        if(status >= 200 && status < 300) {
-            productDispatch({
-                type: 'INIT_PRODUCTS_SUCCESS', 
-                payload: {
-                    products,
-                    error: null,
-                    loading: false
+	try {
+		const {
+			data: { products },
+		} = await axios.get("/api/products");
+
+		productDispatch({
+			type: "INIT_PRODUCTS_SUCCESS",
+			payload: {
+				products,
+				productsMessages: {
+					error: null,
+					loading: false,
+				},
+			},
+		});
+	} catch (error) {
+		productDispatch({
+			type: "INIT_PRODUCTS_FAILURE",
+			payload: {
+                productsMessages: {
+                    error: "Products could not be fetched. Try again later.",
+                    loading: false,
                 }
-            });
-            return;
-        }
-        productDispatch({
-            type: 'INIT_PRODUCTS_FAILURE', 
-            payload: {
-                products: [],
-                error: 'Books could not be fetched. Try again after sometime',
-                loading: false,
-            }
-        });
-    }
-    catch(error) {
-        productDispatch({
-            type: 'INIT_PRODUCTS_FAILURE', 
-            payload: {
-                products: [],
-                error: 'Books could not be fetched. Try again after sometime',
-                loading: false,
-            }
-        });
-    }
-}
+			},
+		});
+	}
+};
