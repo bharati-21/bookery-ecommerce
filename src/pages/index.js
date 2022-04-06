60cdc61d8d8b27aadf429d0d402fff81dbe6b6db
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-06 19:12:37 +0530

    feat: created new page to display single product

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { Cart } from "./Cart/Cart";
 export { WishList } from "./WishList/WishList";
 export { Signup } from "./Auth/Signup";
 export { Login } from "./Auth/Login";
+export { ProductPageItem } from './ProductPage/ProductPageItem';
