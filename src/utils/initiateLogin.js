46b0f7eddec4c022c50ae5d7d7fae644e424096a
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-07 17:21:24 +0530

    refactor: edit initiate login call

--- src/utils/initiateLogin.js
+++ src/utils/initiateLogin.js
@@ -1,20 +1,5 @@
 import axios from 'axios';
-import { toast } from 'react-toastify';
 
-
-const initiateLogin = async (formData) => {
-    try {
-        const { status, data }  = await axios.post('/api/auth/login', formData);
-        if(status >= 200 && status < 300) {
-            return data;
-        }
-        else  {
-            throw new Error('Login failed');
-        }
-    }
-    catch(error) {
-        return false;
-    }
-}
+const initiateLogin = async formData => await axios.post('/api/auth/login', formData);
 
 export { initiateLogin };
