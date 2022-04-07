438d7035f50eae16b3a009d8a80417b660655629
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-07 17:47:54 +0530

    refactor: edit signup, login calls

--- src/utils/initiateLogin.js
+++ src/utils/initiateLogin.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const initiateLogin = async formData => await axios.post('/api/auth/login', formData);
+const initiateLogin = formData => axios.post('/api/auth/login', formData);
 
 export { initiateLogin };
--- src/utils/initiateSignup.js
+++ src/utils/initiateSignup.js
@@ -1,23 +1,11 @@
 import axios from 'axios';
 
-const initiateSignup = async ({firstName, lastName, email, password}) => {
-    try {
-        const { status, data } = await axios.post('/api/auth/signup', {
+const initiateSignup = ({firstName, lastName, email, password}) => axios.post('/api/auth/signup', {
             firstName,
             lastName,
             email, 
             password
         });
-        if(status >= 200 && status < 300) {
-            return data;
-        }
-        else {
-            throw new Error('Sign up Failed');
-        }
-    }
-    catch(error) {
-        return false;
-    }
-}
+
 
 export { initiateSignup };
