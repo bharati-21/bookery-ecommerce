b4ae0c5da443e301aeff0c926ae5e7f824ec6bc1
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-04-07 21:24:24 +0530

    feat: Added not found page (#18)
    * feat: not found page
    
    * fix: object fit

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,21 @@
+import NotFoundImage from "assets/images/not_found.svg";
+import { Link } from "react-router-dom";
+import "./not-found.css";
+
+const NotFound = () => {
+	return (
+		<main className="main not-found-main p-2 flex-col flex-align-center flex-justify-center">
+			<h1>Page Not Found!</h1>
+			<button className="btn btn px-1 py-0-25 text-lg">
+				<Link to="/">Go Back Home</Link>
+			</button>
+			<img
+				src={NotFoundImage}
+				alt="Not Found Image"
+				className="img-responsive not-found-img"
+			/>
+		</main>
+	);
+};
+
+export { NotFound };
