16cccf6d575a18496611cf4f532778598990f3f5
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-07 16:47:01 +0530

    refactor: book length, no books message

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,18 +5,30 @@ const ProductList = ({ products }) => {
     const sortedFilteredBooks = compose(products);  
 
     return (
-        <section className="product-catalog">
-            <p className="text-reg mb-1">Number of Books: <span className="primary-color">{sortedFilteredBooks.length}</span></p>
-            <article className="products-container grid grid-autofit">
-                {
-                    sortedFilteredBooks.length > 0 ?
-                    sortedFilteredBooks.map(book => <ProductItem book={book} key={book._id} />)
-                        :
-                    <h2 className="my-1 text-center error-color">No Books Found!</h2>
+		<section className="product-catalog">
+			<article className="products-container grid grid-autofit">
+				{
+                    sortedFilteredBooks.length > 0 ? (
+                        <>
+                            <p className="text-reg mb-1 grid-row-span text-center">
+                                Number of Books:{" "}
+                                <span className="primary-color">
+                                    {sortedFilteredBooks.length}
+                                </span>
+                            </p>
+                            {sortedFilteredBooks.map((book) => (
+                                <ProductItem book={book} key={book._id} />
+                            ))}
+                        </>
+                    ) : (
+                        <h2 className="my-1 text-center error-color grid-row-span">
+                            No Books Found!
+                        </h2>
+                    )
                 }
-            </article>
-        </section>
-    )
+			</article>
+		</section>
+	);
 }
 
 export { ProductList };
