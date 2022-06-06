afa82db26413b2f5578f4deba923ea5a8136f058
Bharati Subramanian <bharatisharada@gmail.com>
2022-06-06 11:50:56 +0530

    refactor: formatting

--- src/components/index.js
+++ src/components/index.js
@@ -1,25 +1,25 @@
-export { Filters } from './Filters/Filters';
-export { Footer } from './Footer/Footer';
-export { Header } from './Header/Header';
-export { Navbar } from './Navbar/Navbar';
-export { ProductItem } from './ProdutList/ProductItem/ProductItem';
-export { ProductList } from './ProdutList/ProductList';
-export { SortingOptions } from './SortingOptions/SortingOptions';
+export { Filters } from "./Filters/Filters";
+export { Footer } from "./Footer/Footer";
+export { Header } from "./Header/Header";
+export { Navbar } from "./Navbar/Navbar";
+export { ProductItem } from "./ProdutList/ProductItem/ProductItem";
+export { ProductList } from "./ProdutList/ProductList";
+export { SortingOptions } from "./FilterOptions/SortingOptions/SortingOptions";
 
-export { BookTypeFilterOption } from './FilterOptions/BookTypeFilters/BookTypeFilterOption'
-export { BookTypeFilters } from './FilterOptions/BookTypeFilters/BookTypeFilters';
+export { BookTypeFilterOption } from "./FilterOptions/BookTypeFilters/BookTypeFilterOption";
+export { BookTypeFilters } from "./FilterOptions/BookTypeFilters/BookTypeFilters";
 
-export { RatingFilterOption } from './FilterOptions/RatingFilters/RatingFilterOption';
-export { RatingFilters } from './FilterOptions/RatingFilters/RatingFilters';
+export { RatingFilterOption } from "./FilterOptions/RatingFilters/RatingFilterOption";
+export { RatingFilters } from "./FilterOptions/RatingFilters/RatingFilters";
 
-export { GenreFilterOption } from './FilterOptions/GenreFilters/GenreFilterOption';
-export { GenreFilters } from './FilterOptions/GenreFilters/GenreFilters';
+export { GenreFilterOption } from "./FilterOptions/GenreFilters/GenreFilterOption";
+export { GenreFilters } from "./FilterOptions/GenreFilters/GenreFilters";
 
-export { PriceFilters } from './FilterOptions/PriceFilters/PriceFilters';
-export { ClearFilters } from './FilterOptions/ClearFilters';
-export { CartList }  from './Cart/CartList';
-export { CartSummary } from './Cart/CartSummary';
+export { PriceFilters } from "./FilterOptions/PriceFilters/PriceFilters";
+export { ClearFilters } from "./FilterOptions/ClearFilters";
+export { CartList } from "./Cart/CartList";
+export { CartSummary } from "./Cart/CartSummary";
 
-export { WishListItem } from './WishList/WishListItem';
+export { WishListItem } from "./WishList/WishListItem";
 
-export { NotFound } from './NotFound/NotFound';
+export { NotFound } from "./NotFound/NotFound";
