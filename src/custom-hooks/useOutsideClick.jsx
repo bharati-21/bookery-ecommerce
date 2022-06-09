b1d96da492d7ceeab96bef5b1667d2b86c46e165
Bharati Subramanian <bharatisharada@gmail.com>
2022-06-09 08:22:53 +0530

    fix: return if ref or current is null from outside click

--- src/custom-hooks/useOutsideClick.jsx
+++ src/custom-hooks/useOutsideClick.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 
 const useOutsideClick = (ref, handler) => {
 	const handleOutsideClick = (event) => {
+		if (!ref || !ref.current) return;
 		if (
 			(ref.current && ref.current === event.target) ||
 			ref.current.contains(event.target)
