224ea505f0f0c3d83a645d45962c0eb15660ac52
Bharati Subramanian <bharatisharada@gmail.com>
2022-06-09 08:30:03 +0530

    feat: add profile routes

--- src/routes/WebsiteRoutes.jsx
+++ src/routes/WebsiteRoutes.jsx
@@ -1,31 +1,45 @@
 /* External imports */
-import { Routes, Route } from 'react-router-dom';
-import Mockman from 'mockman-js';
+import { Routes, Route, Outlet } from "react-router-dom";
+import Mockman from "mockman-js";
 
 /* Page imports */
-import { LandingPage, Products, Cart, WishList, Signup, Login, ProductPageItem } from 'pages/';
-import { ProtectedRoutes } from './ProtectedRoutes';
-import { NotFound } from 'components';
+import {
+	LandingPage,
+	Products,
+	Cart,
+	WishList,
+	Signup,
+	Login,
+	ProductPageItem,
+	Profile,
+	ProfileDetails,
+	Address,
+} from "pages/";
+import { ProtectedRoutes } from "./ProtectedRoutes";
+import { NotFound } from "components";
 
 const WebsiteRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<LandingPage />} /> 
-            <Route path="/home" element={<LandingPage />} /> 
-            <Route path="/products" element={<Products />} />
-            <Route path="/products/:productId" element={<ProductPageItem />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/mockman" element={<Mockman />} /> 
-            <Route path="/" element={<LandingPage />} /> 
-            <Route path="*" element={<NotFound />} />
-            <Route path="/" element={<ProtectedRoutes />}>
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/wishlist" element={<WishList />} />
-                <Route path="/account" />
-            </Route>
-        </Routes>               
-    );
-}
+	return (
+		<Routes>
+			<Route path="/" element={<LandingPage />} />
+			<Route path="/home" element={<LandingPage />} />
+			<Route path="/products" element={<Products />} />
+			<Route path="/products/:productId" element={<ProductPageItem />} />
+			<Route path="/signup" element={<Signup />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/mockman" element={<Mockman />} />
+			<Route path="/" element={<LandingPage />} />
+			<Route path="*" element={<NotFound />} />
+			<Route path="/" element={<ProtectedRoutes />}>
+				<Route path="/cart" element={<Cart />} />
+				<Route path="/wishlist" element={<WishList />} />
+				<Route path="/profile" element={<Profile />}>
+					<Route path="" element={<ProfileDetails />} />
+					<Route path="address" element={<Address />} />
+				</Route>
+			</Route>
+		</Routes>
+	);
+};
 
 export { WebsiteRoutes as Routes };
