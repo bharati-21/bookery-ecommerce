b22f3b9ed17746a215abd218c9511e65d2a7e42a
Bharati Subramanian <bharatisharada@gmail.com>
2022-06-09 08:23:14 +0530

    feat: context for address

---
+++ src/contexts/address-context.js
@@ -0,0 +1,64 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+
+import { addressReducerFunction } from "reducers";
+import { getAllAddresses } from "utils";
+import { useAuth } from "./";
+
+const initialAddressState = {
+	addresses: [],
+	addressLoading: true,
+	addressError: null,
+	addressFormModalVisibility: false,
+	addressToEdit: null,
+};
+
+const AddressContext = createContext(initialAddressState);
+const { Provider } = AddressContext;
+
+const AddressProvider = ({ children }) => {
+	const {
+		authState: { isAuth, token },
+	} = useAuth();
+
+	const fetchAddresses = async () => {
+		try {
+			const {
+				data: { address },
+			} = await getAllAddresses(token);
+			addressDispatch({
+				type: "SET_ADDRESSES",
+				payload: { addresses: address },
+			});
+			addressDispatch({
+				type: "SET_ADDRESS_LOADER_ERROR",
+				payload: { loading: false, error: null },
+			});
+		} catch (error) {
+			addressDispatch({
+				type: "SET_ADDRESS_LOADER_ERROR",
+				payload: { loading: false, error },
+			});
+		}
+	};
+
+	useEffect(() => {
+		if (isAuth) {
+			fetchAddresses();
+		}
+	}, [isAuth]);
+
+	const [addressState, addressDispatch] = useReducer(
+		addressReducerFunction,
+		initialAddressState
+	);
+
+	return (
+		<Provider value={{ ...addressState, addressDispatch }}>
+			{children}
+		</Provider>
+	);
+};
+
+const useAddress = () => useContext(AddressContext);
+
+export { useAddress, AddressProvider };
--- src/contexts/index.js
+++ src/contexts/index.js
@@ -3,3 +3,4 @@ export { useFilter, FilterProvider } from "./filter-context";
 export { useAuth, AuthProvider } from "./auth-context";
 export { useCart, CartProvider } from "./cart-context";
 export { useWishList, WishListProvider } from "./wishlist-context";
+export { useAddress, AddressProvider } from "./address-context";
