8b0ae2b90cc342a6d75d693c1f31dd817320e0b6
Bharati Subramanian <bharatisharada@gmail.com>
2022-06-10 08:27:18 +0530

    feat: view coupon options component

---
+++ src/pages/Cart/CouponOptions.jsx
@@ -0,0 +1,29 @@
+import { useCart } from "contexts";
+import React from "react";
+import { getCartItemsData } from "utils";
+
+const CouponOptions = () => {
+	const { cartDispatch } = useCart();
+
+	const handleShowViewCouponsModal = (event) => {
+		event.stopPropagation();
+		cartDispatch({
+			type: "SET_COUPON_OPTIONS_MODAL_VISIBILITY",
+			payload: { modalVisibility: true },
+		});
+	};
+
+	return (
+		<div className="coupon-options-container py-1-5 flex-row flex-justify-between flex-align-center">
+			<h6 className="coupon-options-head">Coupons available</h6>
+			<button
+				className="btn btn-primary p-0-25"
+				onClick={handleShowViewCouponsModal}
+			>
+				View Coupons
+			</button>
+		</div>
+	);
+};
+
+export { CouponOptions };
