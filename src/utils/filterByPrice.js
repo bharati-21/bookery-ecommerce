bdab47f8325f91096b43bf46e2e0bba766047675
Bharati Subramanian <bharatisharada@gmail.com>
2022-06-10 08:28:32 +0530

    feat: get dynamic sp

--- src/utils/filterByPrice.js
+++ src/utils/filterByPrice.js
@@ -1,3 +1,9 @@
-const filterByPrice = (genreFilteredBooks, maxPriceValue) => genreFilteredBooks.filter(({ sellingPrice }) => sellingPrice <= maxPriceValue);
+import { getSellingPrice } from "./getSellingPrice";
+
+const filterByPrice = (genreFilteredBooks, maxPriceValue) =>
+	genreFilteredBooks.filter(
+		({ originalPrice, discountPercent }) =>
+			getSellingPrice(originalPrice, discountPercent) <= maxPriceValue
+	);
 
 export { filterByPrice };
--- src/utils/getCartItemsTotal.js
+++ src/utils/getCartItemsTotal.js
@@ -1,23 +1,42 @@
-const getCartItemsTotal = (cartItems, sumType) => {
-    switch (sumType) {
-        case 'TOTAL_SELLING_PRICE':
-            return cartItems.reduce((previousCartItemsTotal, { sellingPrice, qty }) => (
-                sellingPrice + previousCartItemsTotal) * qty, 
-                0
-            );
-        case 'TOTAL_COST_PRICE':
-            return cartItems.reduce((previousCartItemsTotal, { originalPrice, qty }) => ( 
-                previousCartItemsTotal + originalPrice) * qty, 
-            0);
-        case 'TOTAL_DISCOUNT_PRICE':
-            return cartItems.reduce((previousCartItemsTotal, { discountPrice, qty }) => (
-                previousCartItemsTotal + discountPrice) * qty,
-            0);
-        case 'TOTAL_ITEMS':
-            return cartItems.reduce((previousCartItemsQuantity, { qty }) => previousCartItemsQuantity + qty, 0);
-        default:
-            return cartItems.reduce((previousCartItemsQuantity, { qty }) => previousCartItemsQuantity + qty, 0);
-    }
+import { getSellingPrice } from "./getSellingPrice";
 
-}
+const getCartItemsTotal = (cartItems, sumType) => {
+	switch (sumType) {
+		case "TOTAL_SELLING_PRICE":
+			return cartItems.reduce(
+				(
+					previousCartItemsTotal,
+					{ originalPrice, discountPercent, qty }
+				) =>
+					(getSellingPrice(originalPrice, discountPercent) +
+						previousCartItemsTotal) *
+					qty,
+				0
+			);
+		case "TOTAL_COST_PRICE":
+			return cartItems.reduce(
+				(previousCartItemsTotal, { originalPrice, qty }) =>
+					(previousCartItemsTotal + originalPrice) * qty,
+				0
+			);
+		case "TOTAL_DISCOUNT_PRICE":
+			return cartItems.reduce(
+				(previousCartItemsTotal, { discountPrice, qty }) =>
+					(previousCartItemsTotal + discountPrice) * qty,
+				0
+			);
+		case "TOTAL_ITEMS":
+			return cartItems.reduce(
+				(previousCartItemsQuantity, { qty }) =>
+					previousCartItemsQuantity + qty,
+				0
+			);
+		default:
+			return cartItems.reduce(
+				(previousCartItemsQuantity, { qty }) =>
+					previousCartItemsQuantity + qty,
+				0
+			);
+	}
+};
 export { getCartItemsTotal };
