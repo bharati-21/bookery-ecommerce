1a722874ce33f8389c7cab9c64c64db49d33d13c
Bharati Subramanian <bharatisharada@gmail.com>
2022-06-15 13:14:31 +0530

    feat: orders, order item page

---
+++ src/components/OrderItem/index.jsx
@@ -0,0 +1,119 @@
+import { AddressItem } from "pages";
+import React from "react";
+import { Link } from "react-router-dom";
+import { getSellingPrice } from "utils";
+
+const OrderItem = ({ order, page }) => {
+	const { orderId, price, selectedCoupon, createdAt, items, address } = order;
+	const getFormattedPrice = (price) =>
+		price.toLocaleString("en-IN", {
+			minimumFractionDigits: 2,
+			maximumFractionDigits: 2,
+		});
+
+	const getFormattedSellingPrice = (originalPrice, discountPercent) => {
+		const sellingPrice = getSellingPrice(originalPrice, discountPercent);
+
+		return getFormattedPrice(sellingPrice);
+	};
+
+	return (
+		<section className={`order-wrapper mx-auto ${page === "orders" ? 'border-bottom py-3': 'border-none'}`}>
+			{Object.keys(order)?.length ? (
+				<div className="order-details mx-auto text-left">
+					<div className="order-summary p-0-75 flex-col flex-align-start flex-justify-start">
+						
+						<div className="order-id flex-row flex-align-start flex-justify-start">
+							<h6 className="text-reg">Order Id:</h6>
+							<p>{orderId}</p>
+						</div>
+
+						<div className="order-total flex-row">
+							<h6 className="text-reg order-total-head">
+								Order Total:
+							</h6>
+							<p>₹ {getFormattedPrice(price)}</p>
+						</div>
+						{selectedCoupon ? (
+							<div className="selected-coupon flex-row">
+								<h6 className="text-reg order-total-head">
+									Discount Applied:
+								</h6>
+								<p>{selectedCoupon?.discount}%</p>
+							</div>
+						) : null}
+						<div className="order-total flex-row">
+							<h6 className="text-reg order-date-head">
+								Order Date:
+							</h6>
+							<p>{new Date(createdAt).toLocaleString()}</p>
+						</div>
+						<div className="shipping-address">
+							<h6 className="text-reg shipping-address-head">
+								Ship to
+							</h6>
+							<AddressItem
+								address={address}
+								page={"orderSummary"}
+							/>
+						</div>
+					</div>
+					<div className="order-items flex-col flex-justify-start flex-align-center">
+						{items.map((item) => (
+							<Link
+								to={`/products/${item?.id}`}
+								className="item card card-horizontal p-0-75"
+								key={item?.id}
+							>
+								<div className="card-description flex-col flex-align-start flex-justify-center">
+									<div className="flex-col mb-0-75">
+										<h6 className="text-reg">
+											{item?.title}
+										</h6>
+										<h6 className="text-xs gray-color">
+											{item?.author}
+										</h6>
+									</div>
+									<p className="text-sm flex-row">
+										<span>Quantity:</span>
+										<span>{item?.qty}</span>
+									</p>
+									<p className="text-sm flex-row">
+										<span>Item Price:</span>
+										<span>
+											₹
+											<span>
+												{getFormattedSellingPrice(
+													item?.originalPrice,
+													item?.discountPercent
+												)}
+											</span>
+										</span>
+									</p>
+								</div>
+								<div className="card-header">
+									<img
+										src={item?.coverImg}
+										alt={`${item?.title} cover`}
+									/>
+								</div>
+							</Link>
+						))}
+					</div>
+				</div>
+			) : (
+				<div className="text-center mt-2">
+					<h4>Order not found!</h4>
+					<Link
+						to="/products"
+						className="btn btn-primary p-0-25 mx-auto mt-1"
+					>
+						Shop more!
+					</Link>
+				</div>
+			)}
+		</section>
+	);
+};
+
+export { OrderItem };
---
+++ src/pages/Orders/index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useOrders } from "contexts";
+import loadingImage from "assets/images/loader.svg";
+import { OrderItem } from "components";
+import { Link } from "react-router-dom";
+
+const Orders = () => {
+	const { orders, ordersLoading, ordersError } = useOrders();
+
+	return (
+		<main className="main order-summary-main my-2 mx-auto px-3 py-2">
+			{ordersLoading ? (
+				<img
+					src={loadingImage}
+					alt="Loading svg"
+					className="img img-responsive mx-auto"
+				/>
+			) : ordersError ? (
+				<h1 className="error text error-color my-2 text-center">
+					{ordersError}
+				</h1>
+			) : (
+				<>
+					<div className="text-center">
+						<h2 className="main-head mb-2">My Orders</h2>
+					</div>
+
+					{orders?.length ? (
+						<div className="orders-list flex-col flex-justify-start flex-align-center">
+							{orders?.map((order) => (
+								<OrderItem
+									order={order}
+									key={order.orderId}
+									page={"orders"}
+								/>
+							))}
+						</div>
+					) : (
+						<div className="mx-auto text-center">
+							<h5>No orders found!</h5>
+						</div>
+					)}
+					<Link
+						to="/products"
+						className="btn btn-primary p-0-25 mt-1 mx-auto"
+					>
+						Shop { orders?.length ? "more" : "now" }
+					</Link>
+				</>
+			)}
+		</main>
+	);
+};
+
+export { Orders };
